fbac53c7c920070bf25a2fe8df0f83e0bd47d3a8
yatheeshraju <yatheesh.konduru@gmail.com>
2024-03-30 23:45:56 +0530

    fix: header bg and vecel spa deep linking

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ export interface HeaderProps {}
 export default function Header(props: HeaderProps) {
   const navigate = useNavigate();
   return (
-    <nav className="flex p-2 sticky top-0 border-b">
+    <nav className="flex p-2 sticky top-0 border-b bg-white dark:bg-zinc-950">
       <span className="flex  justify-between w-full">
         <Link to="/" className="flex items-center gap-2">
           <RocketIcon width={24} height={24} />{' '}
